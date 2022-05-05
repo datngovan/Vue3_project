7c346c7042a4c4b5bfc0f31c0729e3389316fe05
datngovan <s3817813@rmit.edu.vn>
2022-05-05 15:16:43 +0700

    add vue project and Set function

---
+++ src/App.vue
@@ -0,0 +1,110 @@
+<template>
+<body>
+    <section class="cli">
+      <section class="cli-window">
+        <div id="container" v-for="textValue in inputArray" :key="textValue.count">
+          <div>{{textValue.first}}</div>
+          <div>{{textValue.last}}</div>
+        </div>
+      </section>  
+      <form class="cli-input" v-on:submit="load">
+          <input type="text" v-model="input"/>
+      </form>
+  </section>
+</body>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      input: "",
+      inputArray: [],
+      storedValue: {}
+    }
+  },
+  methods: {
+    load(e){
+      e.preventDefault();
+      var inputArray = this.input.split(" ");
+      switch (inputArray[0]) {
+        case "SET":
+          if (3 != inputArray.length) {
+            this.loadInput("ERROR");
+          } else {
+            this.setKey(inputArray[1], inputArray[2]);
+          }
+          break;
+        case "DEL":
+          
+      }
+    },
+    setKey: function (key, itemValue) {
+      this.storedValue[key] = itemValue;
+      this.loadInput("OK");
+    },
+    loadInput(input){
+      let textVal = {};
+      textVal.last = input;
+      textVal.first = this.input;
+      textVal.count = this.inputArray.length + 1;
+      this.inputArray.push(textVal);
+    },
+  }
+}
+</script>
+
+<style>
+html,
+body {
+	height: 100%;
+	width: 100%;
+	margin: 0;
+	padding: 0;
+	font-family: sans-serif;
+	background: #1c1e22;
+}
+body {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+}
+::-webkit-scrollbar {
+	width: 4px;
+  position: sticky;
+}
+::-webkit-scrollbar-thumb {
+	background-color: #4c4c6a;
+	border-radius: 2px;
+}
+.cli {
+	width: 1200px;
+	height: 1200px;
+	max-height: 1000px;
+	display: flex;
+	flex-direction: column;
+	overflow: hidden;
+	box-shadow: 0 0 4px rgba(0, 0, 0, 0.14), 0 4px 8px rgba(0, 0, 0, 0.28);
+}
+.cli-window {
+	flex: auto;
+	max-height: calc(100% - 60px);
+	background: #2f323b;
+	overflow: auto;
+}
+.cli-input input {
+	height: 59px;
+	line-height: 60px;
+	outline: 0 none;
+	border: none;
+	width: 100%;
+	color: white;
+	text-indent: 10px;
+	font-size: 12pt;
+	padding: 0;
+	background: #40434e;
+}
+</style>
